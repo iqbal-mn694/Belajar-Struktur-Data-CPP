0df61db0d909bb3c90f46c1c8c3cd718985851ce
iqbal-mn694 <76202750+iqbal-mn694@users.noreply.github.com>
2021-09-18 20:56:03 +0700

    Menambahkan node baru
    While Loop digunakan untuk memprint node satu persatu persatu

--- 1.Linked List/Linked List.cpp
+++ 1.Linked List/Linked List.cpp
@@ -23,10 +23,38 @@ int main()
   node1->nama = "Iqbal Mutaqin";
   node1->alamat = "Sodonghilir";
   node1->no_hp = 100;
+  node1->next = node2;
+  
+  node2->nama = "Ega";
+  node2->alamat = "Narawita";
+  node2->no_hp = 567;
+  node2->next = node3;
+  
+  node3->nama = "Fadza";
+  node3->alamat = "Ciroda";
+  node3->no_hp = 678;
+  node3->next = NULL;
+  
+  dataSiswa *cur;
+  cur = node1;
+  
+  while (cur!= NULL){
+    cout<<cur->nama<<endl;
+    cout<<cur->alamat<<endl;
+    cout<<cur->no_hp<<endl;  
+    cout<<endl; 
+    
+    cur = cur->next;
+   
+   
+    
+  }
+  
+  
+  
+  
  
-  cout<<node1->nama<<endl;
-  cout<<node1->alamat<<endl;
-  cout<<node1->no_hp<<endl;     
+    
   
   
     
