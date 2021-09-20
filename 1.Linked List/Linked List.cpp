e40fe1ad22fa6fd4c4d7580407208f141526c518
iqbal-mn694 <76202750+iqbal-mn694@users.noreply.github.com>
2021-09-20 15:50:52 +0700

    Menambahkan node dengan Add First

--- 1.Linked List/Linked List.cpp
+++ 1.Linked List/Linked List.cpp
@@ -11,50 +11,91 @@ using namespace std;
       
       
     };
+    dataSiswa *head,*tail,*last;
+    
+    
+void linkedList(string nama,string alamat,int nomer){
+  head = (dataSiswa*) malloc(sizeof(dataSiswa));
+  
+ 
+  
+  head->nama = nama;
+  head->alamat = alamat;
+  head->no_hp = nomer;
+  head->next = NULL;
+  cout<<"babal"<<endl;
+
+}
+void addLast(string nama,string alamat,int nomer){
+  
+  last = (dataSiswa*) malloc(sizeof(dataSiswa));
+  
+ 
+  
+  last->nama = nama;
+  last->alamat = alamat;
+  last->no_hp = nomer;
+  last->next = NULL;
+  tail = last;
+  
+  
+  
+
+}
+void Choose(){
+  if(head->next == NULL){
+     char choice;
+     cout<<"Mau menambah Data(y/n)"<<endl;
+     cin>>choice;
+      
+     
+     switch(choice) {
+       case 'y':
+         head ->next = last;
+         addLast("ega","Tasik",677);
+         cout<<last->nama<<endl;         
+         break;
+       case 'n':
+         head->next = NULL;
+         cout<<head->nama<<endl;
+         cout<<head->alamat<<endl;
+         cout<<head->no_hp<<endl;
+         break;
+       default:
+         cout<<"Pilih antara y/n!";  
+           
+         
+         
+       
+     }
+       
+ 
+    
+    
+  }else{
+    tail = head;
+  }  
+}
+
+
 
 int main()
 {
-  dataSiswa *node1,*node2,*node3;
-  
-  node1 = (dataSiswa*) malloc(sizeof(dataSiswa));
-  node2 = (dataSiswa*) malloc(sizeof(dataSiswa));
-  node3 = (dataSiswa*) malloc(sizeof(dataSiswa));
-  
-  node1->nama = "Iqbal Mutaqin";
-  node1->alamat = "Sodonghilir";
-  node1->no_hp = 100;
-  node1->next = node2;
-  
-  node2->nama = "Ega";
-  node2->alamat = "Narawita";
-  node2->no_hp = 567;
-  node2->next = node3;
-  
-  node3->nama = "Fadza";
-  node3->alamat = "Ciroda";
-  node3->no_hp = 678;
-  node3->next = NULL;
-  
-  dataSiswa *cur;
-  cur = node1;
-  
-  while (cur!= NULL){
-    cout<<cur->nama<<endl;
-    cout<<cur->alamat<<endl;
-    cout<<cur->no_hp<<endl;  
-    cout<<endl; 
-    
-    cur = cur->next;
-   
-   
-    
-  }
+  linkedList("Iqbal","Sodong",455);  
+  Choose();
   
   
   
   
- 
-    
+  
+  
+  
+  
+  
+  
+  
+  
+
   
   
     
